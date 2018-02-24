02c339bd046db3993c52020a25a777d09c60b753
Michael Koukoullis <mike@kouky.org>
2018-02-24 17:31:22 +1100

    Extract user data script

--- main.tf
+++ main.tf
@@ -10,30 +10,7 @@ resource "aws_instance" "swift-server" {
   tags {
     Name = "Swift Server"
   }
-  
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "::Premptively add swift to path::"              
-              echo "export PATH=/opt/swift/usr/bin:\"$${PATH}\"" >> /home/ubuntu/.bashrc
-              
-              echo "::apt-get update::"
-              apt-get -y update
-              
-              echo "::apt-get upgrade::"
-              # https://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
-              DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold"  install grub-pc
-              apt-get -y upgrade
-              
-              echo "::Install Packages::"
-              # https://bugs.swift.org/browse/SR-2743
-              apt-get -y install clang libicu-dev libpython2.7 libcurl3
-              
-              echo "::Download + Install Swift::"
-              wget -qO- https://swift.org/builds/swift-4.0.3-release/ubuntu1604/swift-4.0.3-RELEASE/swift-4.0.3-RELEASE-ubuntu16.04.tar.gz | tar xvz
-              mv swift-4.0.3-RELEASE-ubuntu16.04 /opt/swift              
-              # https://bugs.swift.org/browse/SR-2783
-              chmod a+r /opt/swift/usr/lib/swift/CoreFoundation/*
-              EOF
+  user_data = "${file("user-data.sh")}"
 }
 
 resource "aws_security_group" "swift-server" {
